4be47ab8ef02b750349dda7eeac7c3138c621f8b
zizi0429 <zizi_0429@yahoo.co.jp>
2016-04-03 07:02:01 +0000

    add run script

---
+++ scripts/python-devel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -it \
+  --rm=true \
+  --user=$USER \
+  --volume="/etc/group:/etc/group:ro" \
+  --volume="/etc/passwd:/etc/passwd:ro" \
+  --volume="/etc/shadow:/etc/shadow:ro" \
+  --volume="/home/$USER:/home/$USER" \
+  --volume="/home/$USER:/home/workspace" \
+  zizi0429/python-devel
+
