07bfba52b9fbf9e2ef3df402e8efc1d244695fb7
zizi0429 <zizi_0429@yahoo.co.jp>
2016-04-03 16:51:18 +0900

    add mount sudoders

--- scripts/python-devel.sh
+++ scripts/python-devel.sh
@@ -6,6 +6,7 @@ docker run -it \
   --volume="/etc/group:/etc/group:ro" \
   --volume="/etc/passwd:/etc/passwd:ro" \
   --volume="/etc/shadow:/etc/shadow:ro" \
+  --volume="/etc/sudoers:/etc/sudoers:ro" \
   --volume="/home/$USER:/home/$USER" \
   --volume="/home/$USER:/home/workspace" \
   zizi0429/python-devel
